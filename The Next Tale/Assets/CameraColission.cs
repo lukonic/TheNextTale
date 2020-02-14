12669d3f6f99c999c20c6dd1e80ed5829bf1d455
lukonic <48091019+lukonic@users.noreply.github.com>
2020-02-14 18:22:47 +0200

    Kamera

---
+++ The Next Tale/Assets/CameraColission.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraColission : MonoBehaviour
+{
+
+    public float minDistance = 1.0f;
+    public float maxDistance = 4.0f;
+    public float smooth = 10.0f;
+    Vector3 dollyDir;
+    public Vector3 dollyDirAdjusted;
+    public float distance;
+
+    // Use this for initialization
+    void Awake()
+    {
+        dollyDir = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var d = Input.GetAxis("Mouse ScrollWheel");
+        if (d > 0 && maxDistance > 1.0f)
+        {
+            maxDistance = maxDistance - 1.0f;
+        }
+        if (d < 0 && maxDistance < 4.0f)
+        {
+            maxDistance = maxDistance + 1.0f;
+        }
+        Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir * maxDistance);
+        RaycastHit hit;
+
+        if (Physics.Linecast(transform.parent.position, desiredCameraPos, out hit))
+        {
+            distance = Mathf.Clamp((hit.distance * 0.87f), minDistance, maxDistance);
+
+        }
+        else
+        {
+            distance = maxDistance;
+        }
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDir * distance, Time.deltaTime * smooth);
+    }
+}
