b71e43504e67dfdc777b9d69f6545ca2051a4e62
lukonic <48091019+lukonic@users.noreply.github.com>
2020-02-16 17:05:21 +0200

    Starting area and scripts
    +Starting area
    +Fixed movement
    +Improved animations
    +Script to light up torches
    +Script to pick up keys
    +Script to destroy things

---
+++ The Next Tale/Assets/Scripts/TorchLightUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchLightUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject player;
+    private ParticleSystem ps;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            print("Veikia");
+            transform.GetChild(0).gameObject.SetActive(true);
+            ps = gameObject.GetComponent<ParticleSystem>();
+            var emission = ps.emission;
+            emission.enabled = true;
+        }
+    }
+}
---
+++ The Next Tale/Assets/Scripts/VaseDestroy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VaseDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject player;
+    public GameObject effect;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            Instantiate(effect, transform.position, transform.rotation);
+            print("Veikia");
+            gameObject.SetActive(false);
+
+        }
+    }
+}
