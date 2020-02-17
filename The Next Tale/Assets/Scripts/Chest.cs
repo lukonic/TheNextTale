cfbfb31130a10a7e5c9db2a93fcbb44428f770a9
lukonic <48091019+lukonic@users.noreply.github.com>
2020-02-17 19:07:52 +0200

    Textures, improved barrels, physics for gems

---
+++ The Next Tale/Assets/Scripts/Chest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject player;
+    public GameObject spawn;
+    public int amount;
+    Animator m_Animator;
+    private bool open;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        m_Animator = GetComponent<Animator>();
+        open = false;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player && open == false)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                Instantiate(spawn, transform.position + new Vector3(0, 0.5f), transform.rotation);
+            }
+            
+            m_Animator.SetBool("Atidaryta", true);
+            open = true;
+        }
+    }
+}
