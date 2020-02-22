ce8ce5669d322ba5b3e4da9eb2cda3dd022eb42f
MartynasBoxBox <48065710+MartynasBoxBox@users.noreply.github.com>
2020-02-22 23:27:09 +0200

    Catapult

---
+++ The Next Tale/Assets/Scripts/Catapult.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catapult : MonoBehaviour
+{
+    GameObject player;
+    public bool IsON;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        IsON = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsON)
+        { 
+            GetComponent<Rigidbody>().mass = 75;
+        GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, -15);
+        StartCoroutine(stoparm());
+            IsON = false;
+    }
+    }
+
+    IEnumerator stoparm()
+    {
+        yield return new WaitForSeconds(.3f);
+        GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 3);
+        GetComponent<Rigidbody>().mass = 5;
+    }
+}
---
+++ The Next Tale/Assets/Scripts/Catapult_Button.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catapult_Button : MonoBehaviour
+{
+    GameObject player;
+    GameObject catapult;
+    bool cooldown;
+    public Material[] material;
+    Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        catapult = GameObject.FindGameObjectWithTag("Catapult");
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+        cooldown = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player && (player.GetComponent<Rigidbody>().velocity.y < -2))
+        {
+            if (cooldown == false)
+            {
+                catapult.GetComponent<Catapult>().IsON = true;
+                print("VEIKIA");
+                cooldown = true;
+                rend.sharedMaterial = material[1];
+                StartCoroutine(ExecuteAfterTime(2f));
+            }
+
+        }
+    }
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        cooldown = false;
+        rend.sharedMaterial = material[0];
+    }
+}
